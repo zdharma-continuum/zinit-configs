47334c3de3fb9517204259fa3b1624da7af07ac8
Vladislav Doster <mvdoster@gmail.com>
2022-11-04 22:20:41 -0500

    maint: update plugin
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

---
+++ zinit-configs.plugin.zsh
@@ -0,0 +1,23 @@
+#!/usr/bin/env zsh
+# -*- mode: sh; sh-indentation: 4; sh-basic-offset: 4; -*-
+
+# Copyright (c) 2020 Sebastian Gniazdowski
+# License MIT
+
+# Get $0 according to the Zsh Plugin Standard:
+# https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html#zero-handling
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+CURRENT_DIR=${0:a:h}
+
+zinit-configs-cmd() {
+    "${CURRENT_DIR}"/run.sh
+}
+
+# The subcommand `scope'.
+@zinit-register-annex "zinit-configs" \
+    subcommand:configs \
+    zinit-configs-cmd
+
+# vim:ft=zsh:tw=80:sw=4:sts=4:noet
--- zplugin-configs.plugin.zsh
+++
@@ -1,7 +0,0 @@
-
-# Directory of the script
-CURRENT_DIR=${0:a:h}
-
-zplugin-configs() {
-    "${CURRENT_DIR}"/run.sh
-}
